5fa277170db4eaa8cd9b596015ca8776978a4614
Vosik <vosikvos@gmail.com>
2024-02-12 01:07:03 +0200

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Саша Восколович on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

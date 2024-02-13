cc324615335d07ea5076943cfd7f212d3c0b430f
Vosik <vosikvos@gmail.com>
2024-02-14 00:15:29 +0200

    added Lock View(face id, password)

--- ExpenseTrackerApp/ContentView.swift
+++ ExpenseTrackerApp/ContentView.swift
@@ -11,33 +11,38 @@ struct ContentView: View {
     
     /// - Intro Visibility Status
     @AppStorage("isFirstTime") private var isFirstTime: Bool = true
-    
+    /// App Lock Properties
+    @AppStorage("isAppLockEnabled") private var isAppLockEnabled: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
     /// Active Tab
     @State private var activeTab: Tab = .recents
     
     var body: some View {
-        TabView(selection: $activeTab) {
-            
-            makeTab(for: Recents(), tag: .recents) {
-                Tab.recents.tabContent
+        
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnabled, lockWhenAppGoesBackground: lockWhenAppGoesBackground) {
+            TabView(selection: $activeTab) {
+                
+                makeTab(for: Recents(), tag: .recents) {
+                    Tab.recents.tabContent
+                }
+                
+                makeTab(for: Search(), tag: .search) {
+                    Tab.search.tabContent
+                }
+                
+                makeTab(for: Graphs(), tag: .charts) {
+                    Tab.charts.tabContent
+                }
+                
+                makeTab(for: Settings(), tag: .settings) {
+                    Tab.settings.tabContent
+                }
             }
-            
-            makeTab(for: Search(), tag: .search) {
-                Tab.search.tabContent
+            .tint(appTint)
+            .sheet(isPresented: $isFirstTime) {
+                IntroScreen()
+                    .interactiveDismissDisabled()
             }
-            
-            makeTab(for: Graphs(), tag: .charts) {
-                Tab.charts.tabContent
-            }
-            
-            makeTab(for: Settings(), tag: .settings) {
-                Tab.settings.tabContent
-            }
-        }
-        .tint(appTint)
-        .sheet(isPresented: $isFirstTime) {
-            IntroScreen()
-                .interactiveDismissDisabled()
         }
     }
     
